8213a0ebaedd05604d8a3697bf12a4b426d7b08b
ykn9080 <ykn9080@empal.com>
2021-12-18 18:53:36 +0900

    initial commit

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,30 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","good to see you!!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value="name",required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;
+    }
+}
+
